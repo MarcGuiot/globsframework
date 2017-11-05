f589b797c6cf54c9d8df914a5257c4339db6a85b
Marc Guiot <marc.dudoignon@gmail.com>
2017-11-05 15:37:41 +0100

    Add sample for Readme.md

---
+++ src/test/java/org/globsframework/Sample.java
@@ -0,0 +1,35 @@
+package org.globsframework;
+
+import org.globsframework.metamodel.Field;
+import org.globsframework.metamodel.GlobType;
+import org.globsframework.metamodel.GlobTypeBuilderFactory;
+import org.globsframework.metamodel.annotations.NamingFieldAnnotationType;
+import org.globsframework.metamodel.fields.StringField;
+import org.globsframework.model.MutableGlob;
+
+import static org.junit.Assert.assertEquals;
+
+public class Sample {
+
+    void readme(){
+        GlobType type = GlobTypeBuilderFactory.create("aType")
+            .addIntegerKey("id")
+            .addStringField("string", NamingFieldAnnotationType.UNIQUE_GLOB)
+            .addIntegerField("int")
+            .addLongField("long")
+            .addDoubleField("double")
+            .addBlobField("blob")
+            .addBooleanField("boolean")
+            .get();
+
+        MutableGlob data = type.instantiate();
+
+        StringField stringField = type.getTypedField("string");
+        data.set(stringField, "Hello");
+
+        assertEquals("Hello", data.get(stringField));
+
+        Field namingField = data.getType().findFieldWithAnnotation(NamingFieldAnnotationType.UNIQUE_KEY);
+        assertEquals("Hello", data.getValue(namingField));
+    }
+}
