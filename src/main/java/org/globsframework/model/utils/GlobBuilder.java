8afa08d36358f79c9576fcea24fd9f41575e4779
MarcGuiot <marc.guiot@free.fr>
2018-03-15 22:24:35 +0000

    remove DefaultGlob usage

--- src/main/java/org/globsframework/model/utils/GlobBuilder.java
+++ src/main/java/org/globsframework/model/utils/GlobBuilder.java
@@ -4,7 +4,6 @@ import org.globsframework.metamodel.Field;
 import org.globsframework.metamodel.GlobType;
 import org.globsframework.metamodel.fields.*;
 import org.globsframework.model.*;
-import org.globsframework.model.impl.DefaultGlob;
 import org.globsframework.utils.exceptions.ItemNotFound;
 
 import java.math.BigDecimal;
@@ -12,8 +11,8 @@ import java.time.LocalDate;
 import java.time.ZonedDateTime;
 
 public class GlobBuilder implements FieldValues.Functor, FieldSetter<GlobBuilder>, FieldValues {
-    private FieldValuesBuilder fieldValuesBuilder = new FieldValuesBuilder();
     private GlobType globType;
+    private MutableGlob mutableGlob;
 
     public static GlobBuilder init(GlobType globType) {
         return new GlobBuilder(globType);
@@ -52,113 +51,118 @@ public class GlobBuilder implements FieldValues.Functor, FieldSetter<GlobBuilder
     }
 
     public GlobBuilder set(DoubleField field, Double value) {
-        fieldValuesBuilder.set(field, value);
+        mutableGlob.set(field, value);
         return this;
     }
 
     public GlobBuilder set(DoubleField field, double value) throws ItemNotFound {
-        fieldValuesBuilder.set(field, value);
+        mutableGlob.set(field, value);
         return this;
     }
 
     public GlobBuilder set(DoubleArrayField field, double[] value) throws ItemNotFound {
-        fieldValuesBuilder.set(field, value);
+        mutableGlob.set(field, value);
         return this;
     }
 
     public GlobBuilder set(IntegerField field, Integer value) {
-        fieldValuesBuilder.set(field, value);
+        mutableGlob.set(field, value);
         return this;
     }
 
     public GlobBuilder set(IntegerField field, int value) throws ItemNotFound {
-        fieldValuesBuilder.set(field, value);
+        mutableGlob.set(field, value);
         return this;
     }
 
     public GlobBuilder set(IntegerArrayField field, int[] value) throws ItemNotFound {
-        fieldValuesBuilder.set(field, value);
+        mutableGlob.set(field, value);
         return this;
     }
 
     public GlobBuilder set(StringField field, String value) {
-        fieldValuesBuilder.set(field, value);
+        mutableGlob.set(field, value);
         return this;
     }
 
     public GlobBuilder set(StringArrayField field, String[] value) throws ItemNotFound {
-        fieldValuesBuilder.set(field, value);
+        mutableGlob.set(field, value);
         return this;
     }
 
     public GlobBuilder set(BooleanField field, Boolean value) {
-        fieldValuesBuilder.set(field, value);
+        mutableGlob.set(field, value);
         return this;
     }
 
     public GlobBuilder set(BooleanArrayField field, boolean[] value) throws ItemNotFound {
-        fieldValuesBuilder.set(field, value);
+        mutableGlob.set(field, value);
         return this;
     }
 
     public GlobBuilder set(LongField field, Long value) {
-        fieldValuesBuilder.set(field, value);
+        mutableGlob.set(field, value);
         return this;
     }
 
     public GlobBuilder set(LongArrayField field, long[] value) throws ItemNotFound {
-        fieldValuesBuilder.set(field, value);
+        mutableGlob.set(field, value);
         return this;
     }
 
     public GlobBuilder set(LongField field, long value) throws ItemNotFound {
-        fieldValuesBuilder.set(field, value);
+        mutableGlob.set(field, value);
         return this;
     }
 
     public GlobBuilder set(BigDecimalField field, BigDecimal value) throws ItemNotFound {
-        fieldValuesBuilder.set(field, value);
+        mutableGlob.set(field, value);
         return this;
     }
 
     public GlobBuilder set(BigDecimalArrayField field, BigDecimal[] value) throws ItemNotFound {
-        fieldValuesBuilder.set(field, value);
+        mutableGlob.set(field, value);
         return this;
     }
 
     public GlobBuilder set(BlobField field, byte[] value) {
-        fieldValuesBuilder.set(field, value);
+        mutableGlob.set(field, value);
         return this;
     }
 
     public GlobBuilder set(DateField field, LocalDate value) throws ItemNotFound {
-        fieldValuesBuilder.set(field, value);
+        mutableGlob.set(field, value);
         return this;
     }
 
     public GlobBuilder set(DateTimeField field, ZonedDateTime value) throws ItemNotFound {
-        fieldValuesBuilder.set(field, value);
+        mutableGlob.set(field, value);
         return this;
     }
 
     public GlobBuilder setValue(Field field, Object value) throws ItemNotFound {
-        fieldValuesBuilder.setValue(field, value);
+        mutableGlob.setValue(field, value);
         return this;
     }
 
     public GlobBuilder setObject(Field field, Object objectValue) {
-        fieldValuesBuilder.setValue(field, objectValue);
+        mutableGlob.setValue(field, objectValue);
         return this;
     }
 
     public Glob get() {
-        return new DefaultGlob(globType, fieldValuesBuilder.get());
+        try {
+            return mutableGlob;
+        } finally {
+            mutableGlob = globType.instantiate();
+        }
     }
 
     private GlobBuilder(GlobType globType) {
+        mutableGlob = globType.instantiate();
         this.globType = globType;
         for (Field field : globType.getFields()) {
-            fieldValuesBuilder.setValue(field, field.getDefaultValue());
+            mutableGlob.setValue(field, field.getDefaultValue());
         }
     }
 
@@ -171,110 +175,110 @@ public class GlobBuilder implements FieldValues.Functor, FieldSetter<GlobBuilder
     }
 
     public Object getValue(Field field) throws ItemNotFound {
-        return fieldValuesBuilder.get().getValue(field);
+        return mutableGlob.getValue(field);
     }
 
     public Double get(DoubleField field) throws ItemNotFound {
-        return fieldValuesBuilder.get().get(field);
+        return mutableGlob.get(field);
     }
 
     public double get(DoubleField field, double valueIfNull) throws ItemNotFound {
-        return fieldValuesBuilder.get().get(field, valueIfNull);
+        return mutableGlob.get(field, valueIfNull);
     }
 
     public double[] get(DoubleArrayField field) throws ItemNotFound {
-        return fieldValuesBuilder.get().get(field);
+        return mutableGlob.get(field);
     }
 
     public Integer get(IntegerField field) throws ItemNotFound {
-        return fieldValuesBuilder.get().get(field);
+        return mutableGlob.get(field);
     }
 
     public int get(IntegerField field, int valueIfNull) throws ItemNotFound {
-        return fieldValuesBuilder.get().get(field, valueIfNull);
+        return mutableGlob.get(field, valueIfNull);
     }
 
     public int[] get(IntegerArrayField field) throws ItemNotFound {
-        return fieldValuesBuilder.get().get(field);
+        return mutableGlob.get(field);
     }
 
     public String get(StringField field) throws ItemNotFound {
-        return fieldValuesBuilder.get().get(field);
+        return mutableGlob.get(field);
     }
 
     public String[] get(StringArrayField field) throws ItemNotFound {
-        return fieldValuesBuilder.get().get(field);
+        return mutableGlob.get(field);
     }
 
     public Boolean get(BooleanField field) throws ItemNotFound {
-        return fieldValuesBuilder.get().get(field);
+        return mutableGlob.get(field);
     }
 
     public Boolean get(BooleanField field, boolean defaultIfNull) {
-        return fieldValuesBuilder.get().get(field);
+        return mutableGlob.get(field);
     }
 
     public boolean[] get(BooleanArrayField field) {
-        return fieldValuesBuilder.get().get(field);
+        return mutableGlob.get(field);
     }
 
     public boolean isTrue(BooleanField field) throws ItemNotFound {
-        return fieldValuesBuilder.get().isTrue(field);
+        return mutableGlob.isTrue(field);
     }
 
     public Long get(LongField field) throws ItemNotFound {
-        return fieldValuesBuilder.get().get(field);
+        return mutableGlob.get(field);
     }
 
     public long get(LongField field, long valueIfNull) throws ItemNotFound {
-        return fieldValuesBuilder.get().get(field, valueIfNull);
+        return mutableGlob.get(field, valueIfNull);
     }
 
     public long[] get(LongArrayField field) throws ItemNotFound {
-        return fieldValuesBuilder.get().get(field);
+        return mutableGlob.get(field);
     }
 
     public BigDecimal get(BigDecimalField field) throws ItemNotFound {
-        return fieldValuesBuilder.get().get(field);
+        return mutableGlob.get(field);
     }
 
     public BigDecimal[] get(BigDecimalArrayField field) throws ItemNotFound {
-        return fieldValuesBuilder.get().get(field);
+        return mutableGlob.get(field);
     }
 
     public LocalDate get(DateField field) throws ItemNotFound {
-        return fieldValuesBuilder.get().get(field);
+        return mutableGlob.get(field);
     }
 
     public ZonedDateTime get(DateTimeField field) throws ItemNotFound {
-        return fieldValuesBuilder.get().get(field);
+        return mutableGlob.get(field);
     }
 
     public byte[] get(BlobField field) throws ItemNotFound {
-        return fieldValuesBuilder.get().get(field);
+        return mutableGlob.get(field);
     }
 
     public boolean contains(Field field) {
-        return fieldValuesBuilder.get().contains(field);
+        return mutableGlob.contains(field);
     }
 
     public int size() {
-        return fieldValuesBuilder.get().size();
+        return mutableGlob.size();
     }
 
     public <T extends FieldValues.Functor> T apply(T functor) throws Exception {
-        return fieldValuesBuilder.get().apply(functor);
+        return mutableGlob.apply(functor);
     }
 
     public <T extends FieldValues.Functor> T safeApply(T functor) {
-        return fieldValuesBuilder.get().safeApply(functor);
+        return mutableGlob.safeApply(functor);
     }
 
     public FieldValue[] toArray() {
-        return fieldValuesBuilder.get().toArray();
+        return mutableGlob.toArray();
     }
 
     public String toString() {
-        return fieldValuesBuilder.toString();
+        return mutableGlob.toString();
     }
 }
