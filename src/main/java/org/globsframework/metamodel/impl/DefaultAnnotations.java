32e008ff4a2948d61ea1d5854ceadb8217440c42
MarcGuiot <marc.guiot@free.fr>
2021-01-11 09:51:33 +0100

    remove useless mapOfMap in DefaultAnnotations

--- src/main/java/org/globsframework/metamodel/impl/DefaultAnnotations.java
+++ src/main/java/org/globsframework/metamodel/impl/DefaultAnnotations.java
@@ -6,55 +6,52 @@ import org.globsframework.metamodel.utils.MutableAnnotations;
 import org.globsframework.model.Glob;
 import org.globsframework.model.Key;
 import org.globsframework.model.format.GlobPrinter;
-import org.globsframework.utils.collections.MapOfMaps;
 import org.globsframework.utils.exceptions.ItemNotFound;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Spliterators;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
-
-import static java.util.Spliterator.SIZED;
 
 public class DefaultAnnotations<T extends MutableAnnotations> implements MutableAnnotations<T> {
     static private final Logger LOGGER = LoggerFactory.getLogger(DefaultAnnotations.class);
-    volatile private MapOfMaps<GlobType, Key, Glob> annotations = new MapOfMaps<>();
+    volatile private Map<Key, Glob> annotations = new HashMap<>();
 
     public DefaultAnnotations() {
     }
 
     public DefaultAnnotations(Glob[] annotations) {
         for (Glob annotation : annotations) {
-            this.annotations.put(annotation.getType(), annotation.getKey(), annotation);
+            this.annotations.put(annotation.getKey(), annotation);
         }
     }
 
     public DefaultAnnotations(Annotations annotations) {
         annotations.streamAnnotations()
-            .forEach(annotation -> this.annotations.put(annotation.getType(), annotation.getKey(), annotation)
-            );
+                .forEach(annotation -> this.annotations.put(annotation.getKey(), annotation)
+                );
     }
 
     public T addAnnotation(Glob glob) {
         if (glob != null) {
             synchronized (this) {
-                MapOfMaps<GlobType, Key, Glob> tmp = new MapOfMaps<>(annotations);
-                Glob old = tmp.put(glob.getType(), glob.getKey(), glob);
+                Map<Key, Glob> tmp = new HashMap<>(annotations);
+                Glob old = tmp.put(glob.getKey(), glob);
                 if (old != null && old != glob) {
                     LOGGER.warn(GlobPrinter.toString(glob) + " has replaced " + GlobPrinter.toString(old));
                 }
                 annotations = tmp;
             }
         }
-        return (T)this;
+        return (T) this;
     }
 
     public T addAnnotations(Stream<Glob> globs) {
         synchronized (this) {
-            MapOfMaps<GlobType, Key, Glob> tmp = new MapOfMaps<>(annotations);
+            Map<Key, Glob> tmp = new HashMap<>(annotations);
             globs.forEach(glob -> {
-                Glob old = tmp.put(glob.getType(), glob.getKey(), glob);
+                Glob old = tmp.put(glob.getKey(), glob);
                 if (old != null && old != glob) {
                     LOGGER.warn(GlobPrinter.toString(glob) + " has replaced " + GlobPrinter.toString(old));
                 }
@@ -62,23 +59,23 @@ public class DefaultAnnotations<T extends MutableAnnotations> implements Mutable
 
             annotations = tmp;
         }
-        return (T)this;
+        return (T) this;
     }
 
     public Stream<Glob> streamAnnotations() {
-        return StreamSupport.stream(Spliterators.spliterator(annotations.iterator(), annotations.size(), SIZED), false);
+        return annotations.values().stream();
     }
 
     public Stream<Glob> streamAnnotations(GlobType type) {
-        return annotations.get(type).values().stream();
+        return annotations.values().stream().filter(glob -> glob.getType() == type);
     }
 
     public boolean hasAnnotation(Key key) {
-        return annotations.containsKey(key.getGlobType(), key);
+        return annotations.containsKey(key);
     }
 
     public Glob getAnnotation(Key key) {
-        Glob annotation = annotations.get(key.getGlobType(), key);
+        Glob annotation = annotations.get(key);
         if (annotation == null) {
             throw new ItemNotFound(key.toString());
         }
@@ -86,6 +83,6 @@ public class DefaultAnnotations<T extends MutableAnnotations> implements Mutable
     }
 
     public Glob findAnnotation(Key key) {
-        return annotations.get(key.getGlobType(), key);
+        return annotations.get(key);
     }
 }
