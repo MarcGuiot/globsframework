ae66f9070fa556380c6cde7d5db5fb34efc0d5f1
MarcGuiot <marc.guiot@free.fr>
2021-01-28 14:08:55 +0100

    Use BooleanField as Predicate

--- src/main/java/org/globsframework/metamodel/fields/BooleanField.java
+++ src/main/java/org/globsframework/metamodel/fields/BooleanField.java
@@ -4,9 +4,14 @@ import org.globsframework.metamodel.Field;
 import org.globsframework.model.Glob;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
-public interface BooleanField extends Field, Function<Glob, Boolean> {
+public interface BooleanField extends Field, Function<Glob, Boolean>, Predicate<Glob> {
     default Boolean apply(Glob glob) {
         return glob.get(this);
+    }
+
+    default boolean test(Glob glob) {
+        return glob.isTrue(this);
     }
 }
