98fd67b7aa877ef866c6304b68c8d6ba36516e47
MarcGuiot <marc.guiot@free.fr>
2021-08-23 09:52:29 +0200

    add EnumAnnotation

--- src/main/java/org/globsframework/metamodel/annotations/AllAnnotations.java
+++ src/main/java/org/globsframework/metamodel/annotations/AllAnnotations.java
@@ -5,15 +5,16 @@ import org.globsframework.metamodel.impl.DefaultGlobModel;
 
 public class AllAnnotations {
     static final public GlobModel MODEL =
-          new DefaultGlobModel(DefaultStringAnnotationType.DESC, AutoIncrementAnnotationType.TYPE,
-                ContainmentLinkAnnotationType.DESC, DefaultBooleanAnnotationType.TYPE,
-                DefaultDoubleAnnotationType.DESC, DefaultFieldValueType.TYPE,
-                DefaultIntegerAnnotationType.DESC, DefaultLongAnnotationType.DESC,
-                DefaultStringAnnotationType.DESC, DoublePrecisionAnnotationType.DESC,
-                FieldNameAnnotationType.TYPE, KeyAnnotationType.TYPE,
-                LinkModelNameAnnotationType.TYPE, MaxSizeType.TYPE,
-                IgnoredAnnotationType.TYPE,
-                CommentType.TYPE,
-                NamingFieldAnnotationType.TYPE, RequiredAnnotationType.TYPE,
-                NotUniqueIndexType.TYPE, FunctionalFieldOrderType.TYPE);
+            new DefaultGlobModel(DefaultStringAnnotationType.DESC, AutoIncrementAnnotationType.TYPE,
+                    ContainmentLinkAnnotationType.DESC, DefaultBooleanAnnotationType.TYPE,
+                    DefaultDoubleAnnotationType.DESC, DefaultFieldValueType.TYPE,
+                    DefaultIntegerAnnotationType.DESC, DefaultLongAnnotationType.DESC,
+                    DefaultStringAnnotationType.DESC, DoublePrecisionAnnotationType.DESC,
+                    FieldNameAnnotationType.TYPE, KeyAnnotationType.TYPE,
+                    EnumAnnotationType.TYPE,
+                    LinkModelNameAnnotationType.TYPE, MaxSizeType.TYPE,
+                    IgnoredAnnotationType.TYPE,
+                    CommentType.TYPE,
+                    NamingFieldAnnotationType.TYPE, RequiredAnnotationType.TYPE,
+                    NotUniqueIndexType.TYPE, FunctionalFieldOrderType.TYPE);
 }
---
+++ src/main/java/org/globsframework/metamodel/annotations/EnumAnnotationType.java
@@ -0,0 +1,42 @@
+package org.globsframework.metamodel.annotations;
+
+import org.globsframework.metamodel.Field;
+import org.globsframework.metamodel.GlobType;
+import org.globsframework.metamodel.GlobTypeLoaderFactory;
+import org.globsframework.metamodel.fields.StringArrayField;
+import org.globsframework.model.Glob;
+import org.globsframework.model.Key;
+import org.globsframework.model.MutableGlob;
+
+import java.util.Optional;
+
+public class EnumAnnotationType {
+    public static GlobType TYPE;
+
+    public static StringArrayField NAME;
+
+    public static Key UNIQUE_KEY;
+
+    static {
+        GlobTypeLoaderFactory.create(EnumAnnotationType.class)
+                .register(GlobCreateFromAnnotation.class, annotation -> TYPE.instantiate()
+                        .set(NAME, ((EnumAnnotation) annotation).value()))
+                .load();
+    }
+
+    public static Glob create(EnumAnnotation nameAnnotation) {
+        return create(nameAnnotation.value());
+    }
+
+    public static MutableGlob create(String[] value) {
+        return TYPE.instantiate().set(NAME, value);
+    }
+
+    public static Optional<String[]> listEnums(Field field) {
+        Glob annotation = field.findAnnotation(UNIQUE_KEY);
+        if (annotation != null) {
+            return Optional.of(annotation.get(NAME));
+        }
+        return Optional.empty();
+    }
+}
